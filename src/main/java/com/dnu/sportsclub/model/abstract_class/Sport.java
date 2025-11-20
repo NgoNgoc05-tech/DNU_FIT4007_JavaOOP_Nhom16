3a1c1a524a0eeead604d22bd64633f9ae0425bb7
Quy20061 <tranquy190806@gmail.com>
2025-11-21 02:05:09 +0700

    abstract done

--- src/main/java/com/dnu/sportsclub/model/abstract_class/Sport.java
+++ src/main/java/com/dnu/sportsclub/model/abstract_class/Sport.java
@@ -1,4 +1,35 @@
 package main.java.com.dnu.sportsclub.model.abstract_class;
 
-public class Sport {
+import java.io.Serializable;
+
+public abstract class Sport implements Serializable {
+    private String id;
+    private String name;
+    private String coachId;
+
+    public Sport(String id, String name, String coachId) {
+        this.id = id;
+        this.name = name;
+        this.coachId = coachId;
+    }
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCoachId() {
+        return coachId;
+    }
+
+    public void setCoachId(String coachId) {
+        this.coachId = coachId;
+    }
+    public abstract String getTrainingDescription();
+    @Override
+    public String toString() {
+        return String.format("Sport[ID=%s, Name=%s, CoachId=%s]", id, name, coachId);
+    }
 }
